f36575a31fbc478a511660bae562026eb5edb8b0
Hassan <hasanahmad9812021@gmail.com>
2024-01-29 20:08:53 +0600

    Teacher group members post has been done

--- src/models/CourseTeacherGroupModel.js
+++ src/models/CourseTeacherGroupModel.js
@@ -1,12 +1,23 @@
 const  mongoose=require('mongoose');
+
+const ChatSchema = mongoose.Schema({
+    message: { type: String },
+    sender: { type: String },
+    //timestamp: { type: Date, default: Date.now }
+}, { versionKey: false });
+
 const DataSchema=mongoose.Schema({
     batch:{type:String},
     section:{type:String},
     courseCode:{type:String},
     courseTitle:{type:String},
     email:{type:String},
+    chat:[ChatSchema],
     createdDate:{type:Date,default:Date.now()}
 },{versionKey:false});
+
 const CourseTeacherGroupModel=mongoose.model('CourseTeacherGroupModel',DataSchema);
-module.exports=CourseTeacherGroupModel
+const ChatModel = mongoose.model('ChatModel', ChatSchema);
+module.exports = CourseTeacherGroupModel
+
 
