b9a55dd945a598aa0a097e514d5d3d7660b2ba46
Hassan <hasanahmad9812021@gmail.com>
2024-01-31 21:40:21 +0600

    Teacher group members checking has been done

--- src/controllers/TeacherTaskController.js
+++ src/controllers/TeacherTaskController.js
@@ -30,7 +30,7 @@ exports.createGroup = async (req, res) => {
         let name = reqBody.member.name
         let bool = false;
         console.log(name)
-        let result2 = await CourseTeacherGroupModel.find({"member.name": name}).count(); // ekane kam roise
+        let result2 = await CourseTeacherGroupModel.find({"member.name": name}).count(); // ekane kam roise korar baki
         console.log('se',result2)
         if(result2 === 0){
             reqBody.email = req.headers['email'];
